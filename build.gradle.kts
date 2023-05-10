d032b5e4242c75b64f94cc9261e143ba584fe202
SirBlobman <blobmansrealm@gmail.com>
2023-05-10 15:01:53 -0400

    Update build scripts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,24 +2,25 @@ plugins {
     id("java")
 }
 
-version="3.0.0"
-
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 tasks {
     named<Jar>("jar") {
+        archiveBaseName.set("checker")
+        version = "3.0.0"
+
         manifest {
             attributes["Main-Class"] = "xyz.sirblobman.application.hash.FileHashChecker"
         }
-
-        archiveBaseName.set("checker")
     }
 
     withType<JavaCompile>() {
         options.encoding = "UTF-8"
         options.compilerArgs.add("-Xlint:deprecation")
+        options.compilerArgs.add("-Xlint:unchecked")
     }
 }
